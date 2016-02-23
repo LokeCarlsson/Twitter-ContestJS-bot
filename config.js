e1a57809b40bbefaad47e33301ecacddd1554c8d
Loke Carlsson <lc222ak@student.lnu.se>
2016-02-23 18:06:29 +0100

    Initial plus minor change to readme

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: "",
-    consumer_secret: "",
-    token: "",
-    token_secret: ""
+    consumer_key: "xjoTFe5WDbtxdMtCSmqFj7K0W",
+    consumer_secret: "DsgzNbdHBR1YsS4BTNHRdDSj7y7GkssfmwTSxMWKeH3Nd43rKU",
+    token: "2655908634-8Exccy25wgNempt1ojyk5pcRMm1Yt4CaKhAI0dQ",
+    token_secret: "YtSfRqX8IP0aMl3CS464oMon2q5HfjV37N5kRy9KVAUz7"
 
 }
